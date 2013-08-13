10c2dc609f9b6ffd11b0b290c1f4f512aac1df90
Alexandre Aubin <alexandre.aubin@cern.ch>
2013-08-13 20:52:51 +0200

    Adding aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##############
+# Misc stuff #
+##############
+
+alias ll='ls -l'
+alias grep='grep --color=auto'
+alias top='top -i'
+
+# Get rid of the gnome pop-up when doing git push
+unset SSH_ASKPASS
+
+##############
+# Colored ls #
+##############
+
+alias ls='ls --color=auto'
+LS_COLORS='di=1;34:fi=0:ln=1;31:pi=1;5:so=1;5:bd=1;5:or=1;31:mi=0:ex=1;32:*.rpm=1;90:*.C=1;35:*.cc=1;35:*.h=0;35'
+export LS_COLORS
+
+#######
+# PS1 #
+#######
+
+PS1="[\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]]\n > "
+
+######################
+# Mapping to scripts #
+######################
+
+alias launchScreen='~/littleScripts/launchScreen'
+alias makec='make 2>&1 | python ~/littleScripts/colorGcc.py'
